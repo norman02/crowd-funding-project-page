990ef51e9e5f35407bf8bf392c37e6a80044f11a
John N. Norman <normanj777@gmail.com>
2021-06-24 09:10:55 -0400

    add product array

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        name: 'bamboo stand',
+        pledge: 25,
+        description: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
+        stock: 101,
+        inStock: true,
+    },
+    {
+        name: 'black edition stand',
+        pledge: 75,
+        description: "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+        stock: 64,
+        inStock: true,
+    },
+    {
+        name: 'mahogany special edition',
+        pledge: 200,
+        description: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+        stock: 0,
+        inStock: false,
+    }
+]
